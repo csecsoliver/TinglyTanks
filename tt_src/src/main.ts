a55ce1f5be07486947de9b66f8075f8ca2aed736
csecsoliver <csecs.oliver.elemer@students.jedlik.eu>
2025-06-01 13:52:23 +0200

    assets added

--- tt_src/src/main.ts
+++ tt_src/src/main.ts
@@ -1,35 +1,44 @@
 import { Application, Assets, Sprite } from "pixi.js";
+// @ts-expect-error I don't know how this works, but it works
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Level } from "./level";
 
 (async () => {
   // Create a new application
   const app = new Application();
-
+  // @ts-expect-error I don't know how this works, but it works
+  globalThis.__PIXI_APP__ = app as Application;
   // Initialize the application
   await app.init({ background: "#1099bb", resizeTo: window });
-
   // Append the application canvas to the document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
 
   // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
+  const texture = await Assets.load("/assets/tankBody_red.png");
+  const texture2 = await Assets.load("/assets/tankRed_barrel1.png");
 
   // Create a bunny Sprite
-  const bunny = new Sprite(texture);
+  const tank = new Sprite(texture);
+  const barrel = new Sprite(texture2);
 
   // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
+  tank.anchor.set(0.5);
+  barrel.anchor.set(0.5, 0);
 
   // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
+  tank.position.set(app.screen.width / 2, app.screen.height / 2);
+  barrel.position.set(app.screen.width / 2, app.screen.height / 2);
 
   // Add the bunny to the stage
-  app.stage.addChild(bunny);
+  app.stage.addChild(tank);
+  app.stage.addChild(barrel);
 
   // Listen for animate update
   app.ticker.add((time) => {
     // Just for fun, let's rotate mr rabbit a little.
     // * Delta is 1 if running at 100% performance *
     // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
+    tank.rotation += 0.01 * time.deltaTime;
+    barrel.rotation += 0.01 * time.deltaTime;
   });
 })();
